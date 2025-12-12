99056a40f7af96b9c5d8725e476c064180f80099
AleksoBre <aleksstevancevic@gmail.com>
2025-12-12 13:42:36 +0100

    added to-do list in web.php file

--- user_post_comments/routes/web.php
+++ user_post_comments/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Final goal
+// forma za crud posta, forma za crud komentara
+// 7 ruta
+
+//1. kreiram stranice
+//2. dodam funkcionalnost
+//3. kreiram rute
