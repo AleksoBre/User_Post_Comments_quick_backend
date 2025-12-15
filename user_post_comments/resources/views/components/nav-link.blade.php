3225e574dd96110c6637a792c70425be3d03f401
AleksoBre <aleksstevancevic@gmail.com>
2025-12-15 13:49:32 +0100

    added layout page, nav-link file

---
+++ user_post_comments/resources/views/components/nav-link.blade.php
@@ -0,0 +1,8 @@
+@props(['active' => 'false'])
+
+<a 
+    {{$attributes}}
+    aria-active="{{ $active }}" 
+    class="{{ $active ? 'text-white bg-gray-950/50' : 'text-gray-300 hover:bg-white/5 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">
+    {{ $slot }}
+</a>
--- user_post_comments/routes/web.php
+++ user_post_comments/routes/web.php
@@ -3,16 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    dd('hello');
+    return view('components.layout');
+});
+
+Route::get('/users', function() {
+    dd('ovo je users stranica');
 });
 
 // Final goal
 // forma za crud posta, forma za crud komentara
 // 7 ruta
 
-//1. kreiram stranice
-// index, show, create, edit
 
+
+// 0. resim ono oko current page-a
+
+// 1. kreiram stranice
+// index, show, create, edit
 
 
 
