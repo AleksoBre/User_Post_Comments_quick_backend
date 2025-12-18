5b0236c066b19bf2256d652d53b7fdf672d9959a
AleksoBre <aleksstevancevic@gmail.com>
2025-12-18 11:39:41 +0100

    added current-page support in the navbar

--- user_post_comments/resources/views/components/layout.blade.php
+++ user_post_comments/resources/views/components/layout.blade.php
@@ -20,12 +20,8 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
-              <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-              <a href="#" aria-current="page" class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-              <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Posts</a>
-              <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Users</a>
-
-              {{-- test za navlink --}}
+              <x-nav-link href="/" :active="request()->is('/')" >Home</x-nav-link>
+              <x-nav-link href="/posts" :active="request()->is('/posts')" >Posts</x-nav-link>
               <x-nav-link href="/users" :active="request()->is('/users')" >Users</x-nav-link>
             </div>
           </div>
@@ -35,12 +31,12 @@
 
   <header class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-white">Home page</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-white">{{$header}}</h1>
     </div>
   </header>
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p class="text-white">my content</p>
+      <div class="text-white">{{$slot}}</div>
     </div>
   </main>
 </div>
