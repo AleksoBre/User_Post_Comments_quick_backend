473ed1b27e6ece50a7eea8bf8bb569f091e79bf2
AleksoBre <aleksstevancevic@gmail.com>
2025-12-19 16:07:30 +0100

    added click on author

--- user_post_comments/resources/views/posts/show.blade.php
+++ user_post_comments/resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:header>{{ $post->content }}</x-slot>
 
-    Created by: {{ $post->user->username }}
+    <a href="/users/{{ $post->user->id }}">Created by: {{ $post->user->username }}</a>
     <br><br>
 
     <a class="text-blue-500 text-xl" href="/posts/{{ $post->id }}/comments">Show comments</a>
