f5eb8ad56f1f0944ab543c8816d84d5ee4bf2438
AleksoBre <aleksstevancevic@gmail.com>
2025-12-23 09:20:00 +0100

    added error checking for input fields

--- user_post_comments/resources/views/users/create.blade.php
+++ user_post_comments/resources/views/users/create.blade.php
@@ -2,7 +2,6 @@
 
 <x-slot:header>Create a User</x-slot>
 
-
 <form action="/users/create" method="POST">
     @csrf
     <div class="border-b border-white/10 pb-12">
@@ -10,23 +9,53 @@
       <p class="mt-1 text-sm/6 text-gray-400">This information will be displayed publicly so be careful what you share.</p>
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
         <div class="sm:col-span-4">
           <label for="username" class="block text-sm/6 font-medium text-white">Username</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500">
-              <input id="username" type="text" name="username" placeholder="My silly username" class="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" required />
+              <input 
+              id="username" 
+              type="text" 
+              name="username" 
+              placeholder="My silly username" 
+              class="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" 
+              required/>
             </div>
           </div>
+
+          @error('username')
+          <div class="mt-1 text-red-500">
+            {{ $message }}
+          </div>
+          @enderror
+
+
+
         </div>
+
 
         <div class="sm:col-span-4">
           <label for="email" class="block text-sm/6 font-medium text-white">Email</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500">
-              <input id="email" type="text" name="email" placeholder="test@example.com" class="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" required/>
+              <input 
+              id="email" 
+              type="text" 
+              name="email" 
+              placeholder="test@example.com" 
+              class="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6" 
+              required/>
             </div>
           </div>
+          @error('email')
+          <div class="mt-1 text-red-500">
+            {{ $message }}
+          </div>
+          @enderror
+
         </div>
+        
       </div>
     </div>
 
